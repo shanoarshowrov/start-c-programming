2ced4a8d18dad0a719967b5537ad8d11555bc7aa
Shanoar Showrov <shanoarshowrov@gmail.com>
2025-11-01 13:48:58 +0600

    HOW TO COMMENT IN C

---
+++ CommentinC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+ //single line comment
+ /*multipleline comment
+ how to comment in c language 
+ */
+ printf ("Comment in C"); //print Comment in c 
+    return 0;
+} 
