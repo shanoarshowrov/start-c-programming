16f58bb5185d9c923035982eba40b6e3845ea3bd
Shanoar Showrov <shanoarshowrov@gmail.com>
2025-11-01 14:10:40 +0600

    c variables

---
+++ Variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int mynum=5; //integer type
+ float myfloatnum=4.2;//float type
+ char mychar = 'b';//char type
+
+
+//variables print 
+
+printf("%d\n", mynum);
+printf("%f\n", myfloatnum);
+printf("%c\n", mychar);
+
+    return 0;
+
+}
