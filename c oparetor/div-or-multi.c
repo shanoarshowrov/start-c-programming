9ef0a19e1976024c99fdfc513502c2eada4f8259
Shanoar Showrov <shanoarshowrov@gmail.com>
2025-11-02 20:28:05 +0600

    C operator divition or Multiplication

---
+++ c oparetor/div-or-multi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int a=2;
+ int b=5;
+ int multi;
+ float div;
+ 
+ multi=a*b;// addition
+ div=b/a; //subtraction
+ printf("%d\n",multi);
+ printf("%f",div);
+ return 0;
+
+}
---
+++ c oparetor/sum or sub.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+#include<stdio.h>
+int main()
+{
+ int a=23;
+ int b=5;
+ int sum,sub;
+ sum=a+b;// addition
+ sub=a-b; //subtraction
+ printf("%d\n",sum);
+ printf("%d",sub);
+ return 0;
+
+}
