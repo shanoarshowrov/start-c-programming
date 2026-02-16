48a0e71f862d515069c8147b6f1dea01c20b0e9f
Md Shanoar Showrov <shanoarshowrov@gmail.com>
2026-02-16 22:09:01 +0600

    Update hw-2.c

--- hw-2.c
+++ hw-2.c
@@ -188,5 +188,84 @@ int main() {
         break;
     }
     }
+//7.Write a C program to check whether a character is uppercase or lowercase alphabet
+#include <stdio.h>
+
+int main() {
+	char ch ;
+	scanf("%c",&ch);
+	if (ch>='A'&& ch<='Z' || ch>='a'&& ch<='z')
+	{
+	    
+	
+	switch(ch)
+	{
+	    case 'A':
+		case 'B':
+		case 'C': 
+		case 'D': 
+		case 'E':
+        case 'F': 
+		case 'G':
+		case 'H': 
+		case 'I':
+		case 'J':
+        case 'K': 
+		case 'L':
+		case 'M': 
+		case 'N':
+		case 'O':
+        case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+		case 'T':
+        case 'U': 
+		case 'V':
+		case 'W':
+		case 'X':
+		case 'Y':
+        case 'Z':
+        printf("The character is a lowercase Alphabet.");
+        break;
+	    case 'a':
+		case 'b':
+		case 'c': 
+		case 'd': 
+		case 'e':
+        case 'f': 
+		case 'g':
+		case 'h': 
+		case 'i':
+		case 'j':
+        case 'k': 
+		case 'l':
+		case 'm': 
+		case 'n':
+		case 'o':
+        case 'p':
+		case 'q':
+		case 'r':
+		case 's':
+		case 't':
+        case 'u': 
+		case 'v':
+		case 'w':
+		case 'x':
+		case 'y':
+        case 'z':
+        printf("The character is a lowercase Alphabet.");
+        break;
+        
+        default :
+        printf("The character is NOT an Alphabet.");
+	}
+	}
+    else 
+    printf("Bohot tej ho rahaho");
+	
+
+}
+
 
 
