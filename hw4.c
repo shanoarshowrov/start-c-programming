1c244e01d6342188c8dbd26ae2eac8620271fb72
Md Shanoar Showrov <shanoarshowrov@gmail.com>
2026-04-12 22:54:49 +0600

    Update hw4.c

--- hw4.c
+++ hw4.c
@@ -65,3 +65,63 @@ int main() {
     return 0;
 }
 
+/*input like : Hello World 
+output : Hello
+         world */
+
+
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    char str1[100];
+    printf("Enter a String : ");
+    gets(str1);
+    printf(" ");
+    for(int i=0;i<strlen(str1);i++)
+    {
+        if(str1[i]==' ')
+        {
+         printf("\n");
+        }
+        printf("%c",str1[i]);
+    }
+    
+    return 0;
+}
+
+
+//Check  substring  or not
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100];
+    char str2[100];
+    printf("Enter a String : ");
+    gets(str1);
+    printf("Enter 2nd String :");
+    gets(str2);
+    for (int i = 0; i < strlen(str1); i++)
+    {
+        if (str1[i] >= 65 && str1[i] <= 90)
+            str1[i] = str1[i] + 32;
+    }
+    for (int i = 0; i < strlen(str2); i++)
+    {
+        if (str2[i] >= 65 && str2[i] <= 90)
+            str2[i] = str2[i] + 32;
+    }
+    if (strstr(str1, str2))
+        printf("2nd string sub string of 1st string ");
+    else
+        printf("Not sub string");
+
+    return 0;
+}
+
+
+
+
